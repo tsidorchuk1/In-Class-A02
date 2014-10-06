1f67c48abeff412fa478df091a9e7f811e41bd3a
tsidorchuk1 <tersid@shaw.ca>
2014-10-05 19:43:11 -0600

    added constraint to reservation class

--- eRestaurant Sample/eRestaurant/Entities/Reservation.cs
+++ eRestaurant Sample/eRestaurant/Entities/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,30 @@ namespace eRestaurant.Entities
 {
     public class Reservation
     {
-        // add in constants for valid reservation status values
-        // b= booked, c=complete, n = no show, x=cancelled
+        // constants for valid reservation status values
+        // B = Booked; C = Complete; N = No-show; X = Cancelled
         public const string Booked = "B";
         public const string Complete = "C";
         public const string NoShow = "N";
         public const string Cancelled = "X";
 
         public int ReservationID { get; set; }
+
+        [Required]
+        [StringLength(40)]
         public string CustomerName { get; set; }
         public DateTime ReservationDate { get; set; }
+        [Range(1, 16)]
         public int NumberInParty { get; set; }
+        [StringLength(15)]
         public string ContactPhone { get; set; }
+        // TODO: (in BLL) Validate acceptable values using constants above
+        [Required, StringLength(1, MinimumLength = 1)]
         public string ReservationStatus { get; set; }
+        [StringLength(1)]
         public string EventCode { get; set; }
+
+      
 
         #region Navigation Properties
         public virtual ICollection<Table> Tables { get; set; }
